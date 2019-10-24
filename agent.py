518667dd33416cb4ecb118bf40e3c3f628b94640
rayd512 <rdizon@ualberta.ca>
2019-10-23 22:11:02 -0600

    added some comments

--- agent.py
+++ agent.py
@@ -1,10 +1,15 @@
 import util
 def agentActions(uid, cursor):
+	# Find the agents name
 	cursor.execute('''SELECT fname FROM users where uid =?''', (uid,))
 	agentName = cursor.fetchone()
+	# Personally welcome the agent
 	print("Welcome Agent " + agentName[0])
+	# Prompt the agaent what he'd like to do today
 	action = input("What would you like to do today? Type help for options\n").lower()
+	# Boolean that holds if more actions are to be made
 	newAction = False
+	# Match the input to the appropriate action
 	while True:
 		if action == "regbirth":
 			# Call function from util.py here
